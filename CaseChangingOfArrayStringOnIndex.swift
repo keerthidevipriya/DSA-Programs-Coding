ae489a3cab34de861d68ec41e870844f5ce42671
keerthidevipriya <keerthidevipriya@gmail.com>
2021-10-31 21:15:51 +0530

    CaseSentivityChangeBasedOnIndexInArrOfStr

---
+++ CaseChangingOfArrayStringOnIndex.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+var a = ["keerthi","priya"]
+var arr =  [String]()
+
+for i in a {
+    let ans = i.enumerated().reduce("") { (k,tup) -> String in 
+        return k + (tup.offset % 2 == 0 ? tup.element.lowercased() : tup.element.uppercased())
+    }
+    arr.append(ans)
+}
+
+print(arr)
